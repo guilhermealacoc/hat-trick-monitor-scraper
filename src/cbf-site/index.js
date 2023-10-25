fa36af228d493163fb68538c4ccae396ad82a8bf
Guilherme de Alacoc Aquino <guilhermealacoc@gmail.com>
2023-10-24 23:33:08 -0300

    feat: add initial files, more test need before aprove to main

---
+++ src/cbf-site/index.js
@@ -0,0 +1,18 @@
+import puppeteer from "puppeteer";
+import fs from "fs";
+
+(async () => {
+  // Launch the browser and open a new blank page
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  // Navigate the page to a URL
+  await page.goto("https://google.com/");
+
+  // Set screen size
+  const pagePrint = await page.setViewport({ width: 1080, height: 1024 });
+
+  fs.writeFileSync("index.html", pagePrint);
+
+  await browser.close();
+})();

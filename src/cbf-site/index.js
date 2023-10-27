103355ad596a92a7e63c1b79e722a770204eeee8
Guilherme de Alacoc Aquino <guilhermealacoc@gmail.com>
2023-10-26 22:29:43 -0300

    feat: adicionando headless para teste

--- src/cbf-site/index.js
+++ src/cbf-site/index.js
@@ -3,7 +3,7 @@ import fs from "fs";
 
 (async () => {
   // Launch the browser and open a new blank page
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
 
   // Navigate the page to a URL
